8db198ba04b8c7be6c90be5b515018c562fc749d
pedia <170561+pedia@users.noreply.github.com>
2024-04-28 16:35:41 +0800

    -

--- backtest/main.go
+++ backtest/main.go
@@ -6,13 +6,18 @@ import (
 )
 
 func main() {
-	for n := 30; n < 100; n += 5 {
-		d := grid.OpenCSV("data/BTCUSDT-1m-2024-01.csv")
+	for up := 40000.0; up < 60000; up += 1000 {
+		for n := 30; n < 100; n += 5 {
+			d := grid.OpenCSV("data/BTCUSDT-1m-2024-01.csv")
 
-		t := grid.NewGrid(40000, 43000, n)
-		pnl, err := grid.Run(t, d)
-		fmt.Printf("%02d %8.02f %8.02f %v\n", n, pnl.Fee, pnl.Profit, err)
+			t := grid.NewGrid(40000, up, n)
+			pnl, err := grid.Run(t, d)
+			if pnl.Profit < 10 {
+				continue
+			}
+			fmt.Printf(" - %6.0f %02d %8.02f %8.02f %v\n", up, n, pnl.Fee, pnl.Profit, err)
 
-		d.Close()
+			d.Close()
+		}
 	}
 }
